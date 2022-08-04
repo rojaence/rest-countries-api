861d69fa02ee6cefa8decea281126ba6705de227
rojaence <rojaence@gmail.com>
2022-08-03 23:49:13 -0500

    Inicio de proyecto: rest-countries-api

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: {
+    RouterLink,
+    RouterView,
+  }
+}
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+</style>
