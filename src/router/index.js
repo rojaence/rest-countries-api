16fee92bc58df9a882d6fbe5da7bc2233879c778
rojaence <rojaence@gmail.com>
2022-08-14 22:58:58 -0500

    Modificación de index router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useCountriesStore } from '@/stores/countries'
+import { nextTick } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,12 +27,16 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach((to) => {
+router.beforeEach((to, from, next) => {
   const countriesStore = useCountriesStore()
   if (to.name === 'CountryDetail') {
     if (!countriesStore.hasCountries) {
       return router.push('/');
+    } else {
+      next()
     }
+  } else {
+    next()
   }
 })
 
